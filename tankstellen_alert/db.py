a17b352198cc8765722e8add1d66b160f7c206c5
anwemo <240973301+anwemo@users.noreply.github.com>
2026-05-15 17:18:49 +0200

    feat: add logging to db.py

--- tankstellen_alert/db.py
+++ tankstellen_alert/db.py
@@ -4,18 +4,24 @@ from typing import Optional
 from decimal import Decimal
 from tankstellen_alert.config import engine
 from tankstellen_alert.models import Base, Station, PriceHistory, AlertStation
+import logging
+
+log = logging.getLogger(__name__)
 
 Base.metadata.create_all(engine)
 
 
 def upsert_station(station_id, data):
-    if not station_id or not data:
-        return
+    log.debug("Upserting station {0}", station_id)
+    station_label = f"{data.get('name')}, {data.get('street')} {data.get('houseNumber')}"
     with Session(engine) as session:
         station = session.get(Station, station_id)
         if not station:
             station = Station(id=station_id)
             session.add(station)
+            log.info("Station not found in db, adding: {0}", station_label)
+        else:
+            log.info("Station found in db, updating: {0}", station_label)
         station.name = data.get("name") or station.name
         station.brand = data.get("brand") or station.brand
         station.street = data.get("street") or station.street
@@ -33,8 +39,6 @@ def upsert_station(station_id, data):
 
 
 def add_price_history(station_ids: list, all_stations_prices):
-    if not station_ids:
-        return
     new_prices = []
     with Session(engine, expire_on_commit=False) as session:
         for station_id, prices in all_stations_prices.items():
@@ -46,6 +50,7 @@ def add_price_history(station_ids: list, all_stations_prices):
                 timestamp=datetime.today(),
                 station_id=station_id,
             )
+            log.debug("Added new PriceHistory: {0}", new_price)
             session.add(new_price)
             new_prices.append(new_price)
         session.commit()
@@ -53,6 +58,7 @@ def add_price_history(station_ids: list, all_stations_prices):
 
 
 def get_last_price(station_id: str, gas_type: str) -> Optional[Decimal]:
+    log.debug("Searching previous {0} price for station {1}", gas_type, station_id)
     with Session(engine) as session:
         entry = (
             session.query(PriceHistory)
@@ -62,11 +68,14 @@ def get_last_price(station_id: str, gas_type: str) -> Optional[Decimal]:
             .first()
         )
         if entry is None:
+            log.debug("No previous {0} price in db", gas_type)
             return None
+        log.debug("Returning previous {0} price from db", gas_type)
         return getattr(entry, gas_type)
 
 
 def station_update_needed(station_id):
+    log.debug("Checking if station {0} needs update", station_id)
     with Session(engine) as session:
         station = session.get(Station, station_id)
         if (
@@ -74,18 +83,23 @@ def station_update_needed(station_id):
             or not station.last_updated
             or datetime.today() - station.last_updated > timedelta(days=7)
         ):
+            log.info("Station {0} needs update", station_id)
             return True
+        log.debug("Needs update: False")
         return False
 
 
+# noinspection PyTypeChecker
 def build_alert_station(new_price: PriceHistory, gas_type: str, threshold: Decimal):
+    log.debug("Building AlertStation for station {0}", new_price.station_id)
     with Session(engine) as session:
         station = session.get(Station, new_price.station_id)
         if not station:
+            log.warning("Station {0} not found in db, skipping alert", new_price.station_id)
             return None
         return AlertStation(
-            station_id=station.id,
             gas_type=gas_type,
+            station_id=station.id,
             price=getattr(new_price, gas_type),
             threshold=threshold,
             name=station.name,
